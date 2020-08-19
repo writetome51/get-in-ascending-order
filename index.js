69649b957e1f4555625e73996823cd2992f4f7dd
Steve Thompson <writetome51@gmail.com>
2020-08-19 17:14:26 -0600

    minor change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import {getArrayCopy} from '@writetome51/get-array-copy';
 import TimSort from 'timsort';
-const sort = TimSort.sort;
 
 
 // Returns new array of `numbers` in ascending order.
 
 export function getInNumericOrder(numbers) {
+	const sort = TimSort.sort;
 	let nums = getArrayCopy(numbers);
 	if (nums.length < 2) return nums;
 	sort(nums, (a, b) => a - b);
